2d0778f48083dbf0a28788aa22fab8851f6fd2ec
MoWIllFTCC13 <55112708+MoWIllFTCC13@users.noreply.github.com>
2019-09-09 17:41:37 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_MohammedWilliams.py
@@ -0,0 +1,12 @@
+#This project will calculate predict annual profit compared to total sales
+#September 4th 2019
+#CTI-110 P2T1 - Sales Prediction
+#Mohammed Williams
+
+total_sales = float( input("Enter the projected sales:"))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print("The profit is $", profit)
